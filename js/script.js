573cc478a9663503a13c0202da2711ba689426a4
Jose Munoz <jommunoz90@gmail.com>
2022-10-05 10:27:45 +0100

    nearly finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+    distance: '25px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.home-text', {delay:190, origin:'bottom'})
+
+sr.reveal('.about, .education, .services, .contact',{delay: 200, origin:'bottom'})
